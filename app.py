406bfddfba038c21a425d1fc467ce0bb7229f693
Luke Murphy <lukewm@riseup.net>
2020-08-29 15:28:39 +0200

    First stab at multiprocessing tasks

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
+from multiprocessing import Process
 from os import chdir
+from pathlib import Path
+from shlex import split
 from subprocess import run
 
 from flask import Flask
 
 app = Flask(__name__)
 
+UTILS_PATH = Path("rpi-rgb-led-matrix/utils").absolute()
+
+LED_CMD = f"{UTILS_PATH}/led-image-viewer"
+LED_IMG = f"{UTILS_PATH}/chaturbate.ppm"
+LED_ARGS = [
+    "--led-chain=8",
+    "--led-rows=32",
+    "--led-cols=32",
+    "--led-parallel=1",
+    "--led-gpio-mapping=adafruit-hat",
+    "-f",
+]
+
+
+def run_animation():
+    chdir(UTILS_PATH)
+    cmd = f"{LED_CMD} {" ".join(LED_ARGS)} {LED_IMG}"
+    run(split(cmd))
+
+
+def run_goal_msg():
+    pass
+
+
+animations_process = Process(target=run_animation)
+goal_message_process = Process(target=run_goal_msg)
+
 
 @app.route("/")
-def panel():
-    chdir("/home/decentral1se/rpi-rgb-led-matrix/bindings/python/samples")
-    run("sudo ./runtext.py --led-cols=192 --led-rows=32", shell=True)
+def home():
+    pass
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    animations_process.start()
+    # app.run(debug=True, host="0.0.0.0")
